f098855980147546326a289a490afe4f09fce099
Jacob Alford <jacob.alford@me.com>
2020-03-04 04:09:39 +0000

    Whoopsie

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,4 @@
 import isSquare from "./isSquare/isSquare";
 import checkValidity from "./checkValidity/checkValidity";
 
-export { isSquare, checkValidity, validate };
+export { isSquare, checkValidity };

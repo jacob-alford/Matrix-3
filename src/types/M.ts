28644432c53e5b9131ad91502942e73a4d63f9ea
Jacob Alford <jacob.alford@me.com>
2020-04-14 03:11:57 +0000

    Bolster M

--- src/types/M.ts
+++ src/types/M.ts
@@ -1,7 +1,35 @@
-import { mat, vec } from './data-structures';
-import { checkValidity } from '../utils';
+import { mat, vec, shape as shapeType } from './data-structures';
+import { checkValidity, isMat, isVec } from '../utils';
+import { shape as shapeMat } from 'linearAlgebraUtils';
 
-export const M = (matrix: mat): mat => {
+interface matConstructorOptions {
+  newRowDelimiter?: string;
+  newColDelimiter?: string;
+  shape?: shapeType;
+}
+
+export const M = (item: mat | vec | string, options?: matConstructorOptions): mat => {
+  const { newRowDelimiter = '\n', newColDelimiter = ' ', shape } = options || {};
+  const matrix = ((A: mat | vec | string): mat => {
+    if (typeof A === 'string')
+      return A.split(newRowDelimiter).map(row =>
+        row.split(newColDelimiter).map((item, index) => {
+          const num = +item;
+          if (!isNaN(num)) return num;
+          throw new Error(`Item ${item} at index ${index} is not a number!`);
+        })
+      );
+    if (isMat(A)) return A;
+    if (isVec(A)) {
+      if (!shape)
+        throw new Error(`options.shape is required to construct a matrix from a vector!`);
+      const returnMat = shapeMat(A, shape);
+      if (isVec(returnMat))
+        throw new Error(`options.shape: [${shape.join(',')}] would produce a vector!`);
+      return returnMat;
+    }
+    return A;
+  })(item);
   checkValidity(matrix);
   return matrix;
 };
